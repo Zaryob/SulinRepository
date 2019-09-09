b85f0f81294ddb9a94285f67e8edfebeaa2c91b2
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-10 02:36:52 +0300

    Fixed 32-bit LSB files in /usr/libexec and /usr/bin

--- multimedia/graphics/libjpeg-turbo/actions.py
+++ multimedia/graphics/libjpeg-turbo/actions.py
@@ -6,6 +6,7 @@
 from inary.actionsapi import cmaketools
 from inary.actionsapi import get
 from inary.actionsapi import inarytools
+from inary.actionsapi import autotools
 
 def setup():
     cmaketools.configure("-DWITH_JPEG8=1 ")
@@ -14,8 +15,11 @@ def build():
     cmaketools.make()
 
 def install():
+    if get.buildTYPE() == "emul32":
+        cmaketools.rawInstall("DESTDIR=%s/emul32" % get.installDIR())
+        inarytools.domove("/emul32/usr/lib32", "/usr/")
+        inarytools.removeDir("/emul32")
+        return
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    if get.buildTYPE() == "emul32": return
-
     # provide jpegint.h as it is required by various software
     inarytools.insinto("/usr/lib/include", "jpegint.h")
