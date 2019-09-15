1d667caf3e7a1aac844b99b111ac34d764beb324
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-16 00:42:50 +0300

    Rust is ready to rool. :smile:

---
+++ programming/language/rust/rust-standlone/actions.py
@@ -0,0 +1,12 @@
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
+
+NoStrip = "/"
+
+def install():
+    shelltools.system("./install.sh --destdir={0}/ --prefix={0}/usr \
+                                    --sysconfdir={0}/etc --bindir={0}/usr/bin \
+                                    --libdir={0}/usr/lib --datadir={0}/usr/share \
+                                    --mandir={0}/usr/share/man \
+                                    --docdir={0}/usr/share/doc/ \
+                                    --disable-ldconfig".format(get.installDIR()))
