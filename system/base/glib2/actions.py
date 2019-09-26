0fc3a9ef415dfb96e8878aa4e5a12e446ab14ea1
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-09-26 20:01:56 +0000

    glib2-32bit paketine ait düzenlemeler.

--- system/base/glib2/actions.py
+++ system/base/glib2/actions.py
@@ -6,6 +6,8 @@
 
 from inary.actionsapi import mesontools
 from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
 
 
 def setup():
@@ -15,4 +17,11 @@ def build():
     mesontools.ninja_build()
 
 def install():
-    mesontools.ninja_install()
+    if get.buildTYPE()=="emul32":
+        shelltools.system('DESTDIR="{}/emul32" ninja install -C inaryPackageBuild'.format(get.installDIR()))
+        inarytools.domove("/emul32/usr/lib32", "/usr/lib32")
+        inarytools.domove("/emul32/usr/bin/*", "/usr/bin/32")
+        inarytools.removeDir("/emul32")
+        return
+    else:
+        mesontools.ninja_install()
