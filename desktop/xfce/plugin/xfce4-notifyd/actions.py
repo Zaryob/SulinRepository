8c8534440ce8b7f85a69783dddf70cf182660877
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-10-23 17:19:43 +0300

    XFCE4 Plugins added.
    Some of plugins added to repository.

---
+++ desktop/xfce/plugin/xfce4-notifyd/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    
+    inarytools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    inarytools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
