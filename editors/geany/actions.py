53bbd0f8b41e29782b8a7de0aea3c8daeb6d668c
Ali Rıza KESKİN <953863-sulincix@users.noreply.gitlab.com>
2019-11-09 20:21:40 +0300

    geany tamam

---
+++ editors/geany/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import get
+
+
+def setup():
+	autotools.configure("--prefix=/usr \
+                --enable-gtk3 \
+                --enable-gtkdoc-header")
+	inarytools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+	autotools.make()
+
+def install():
+	inarytools.dodoc('AUTHORS', 'ChangeLog',
+	                'COPYING', 'HACKING', 'INSTALL', 'NEWS', 'README',
+	                'README.I18N', 'README.Packagers', 'THANKS', 'TODO')
+	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
