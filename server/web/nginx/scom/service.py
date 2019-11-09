9783aa6e3d773b86a4603ec7b3e837184ae558d8
Ali Rıza KESKİN <953863-sulincix@users.noreply.gitlab.com>
2019-11-09 16:43:05 +0300

    nginx ekledim ama scom betiğine biraz daha baksak iyi olcak

---
+++ server/web/nginx/scom/service.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+serviceType = "server"
+serviceDesc = _({"en": "Nginx Web Server",
+                 "tr": "Nginx Web Sunucusu"})
+
+from scom.service import *
+
+@synchronized
+def start():
+    startService(command="/usr/sbin/nginx",
+                 donotify=True)
+
+@synchronized
+def stop():
+    stopService(pidfile="/run/nginx.pid",
+                donotify=True)
+
+def status():
+    return isServiceRunning("/run/nginx.pid")
