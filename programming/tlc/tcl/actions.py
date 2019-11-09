cd2b202cb36e9fbbcecb372e9189eb15e86a7b0f
Ali Rıza KESKİN <953863-sulincix@users.noreply.gitlab.com>
2019-11-09 20:23:55 +0300

    tcl ve tcltk eklendi

---
+++ programming/tlc/tcl/actions.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import get
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+
+def setup():
+    shelltools.cd("unix")
+
+    autotools.autoreconf("-fi")
+    autotools.configure("--with-encoding=utf-8 \
+                         --enable-threads \
+                         --enable-man-compression=gzip \
+                         --mandir=/usr/share/man \
+                         --enable-man-symlinks \
+                         --enable-shared \
+                         --enable-64bit")
+
+def build():
+    shelltools.cd("unix")
+    autotools.make()
+
+def install():
+    shelltools.cd("unix")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # Collect private headers, 3rd party apps like Tile depends on this
+    shelltools.cd("..")
+    inarytools.dodir("/usr/include/tcl-private/generic")
+    inarytools.dodir("/usr/include/tcl-private/unix")
+    shelltools.copy("unix/*.h","%s/usr/include/tcl-private/unix" % get.installDIR())
+    shelltools.copy("generic/*.h", "%s/usr/include/tcl-private/generic" % get.installDIR())
+
+    # Remove duplicated headers
+    inarytools.remove("/usr/include/tcl-private/generic/tcl.h")
+    inarytools.remove("/usr/include/tcl-private/generic/tclDecls.h")
+    inarytools.remove("/usr/include/tcl-private/generic/tclPlatDecls.h")
+
+    # Remove man file sqlite paket çakışması için silindi.
+    #inarytools.remove("/usr/share/man/mann/sqlite3.n")
+
+    # Expect package needs these symlinks
+    inarytools.dosym("/usr/include/tcl-private/unix/tclUnixPort.h","/usr/include/tcl-private/generic/tclUnixPort.h")
+    inarytools.dosym("/usr/include/tcl-private/unix/tclUnixThrd.h","/usr/include/tcl-private/generic/tclUnixThrd.h")
+
+    # Remove tmp path from tclConfig.sh
+    inarytools.dosed("%s/usr/lib/tclConfig.sh" % get.installDIR(),"%s/unix" % get.curDIR() ,"/usr/lib/")
+    inarytools.dosed("%s/usr/lib/tclConfig.sh" % get.installDIR(),"%s" % get.curDIR() ,"/usr/include/tcl-private")
+
+    # Some apps need compat headers
+    inarytools.dodir("/usr/include/tcl-private/compat")
+    shelltools.copy("compat/*.h", "%s/usr/include/tcl-private/compat" % get.installDIR())
+
+    inarytools.dosym("/usr/bin/tclsh8.6","/usr/bin/tclsh")
+
+    inarytools.dodoc("ChangeLog","changes","license.terms","README")
