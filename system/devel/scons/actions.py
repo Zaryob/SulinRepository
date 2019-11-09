d5a3546a3ce82ddd6d49babf8fdd277a349d0ed4
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-10 00:35:10 +0300

    Scons python3 ile uyumlu olan versiyona yükseltildi.

--- system/devel/scons/actions.py
+++ system/devel/scons/actions.py
@@ -6,14 +6,17 @@
 
 from inary.actionsapi import pythonmodules
 from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
 
 def build():
-    pythonmodules.compile(pyVer="2")
+    pythonmodules.compile(pyVer="3")
 
 def install():
     pythonmodules.install("--no-version-script \
+                           --optimize=1 \
                            --standalone-lib \
                            --install-scripts=/usr/bin \
-                           --install-data=/usr/share", pyVer="2")
-
+                           --install-data=/usr/share", pyVer="3")
+    shelltools.system("sed -i 's|env python|env python3|' {}/usr/bin/*".format(get.installDIR()))
     inarytools.dodoc("CHANGES*", "LICENSE*", "README*", "RELEASE*")
