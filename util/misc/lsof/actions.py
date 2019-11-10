b06f1cb126be231535641cd0527b52eda80fd5ce
Ali Rıza KESKİN <953863-sulincix@users.noreply.gitlab.com>
2019-11-10 15:22:14 +0300

    miutil/sc eklemeleri yapıldı ()

---
+++ util/misc/lsof/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
+
+WorkDir = "lsof-%s" % get.srcVERSION()
+
+def setup():
+    shelltools.export("LINUX_BASE", "/proc")
+    shelltools.export("LSOF_LDFLAGS", get.LDFLAGS())
+
+    shelltools.touch(".neverInv")
+    shelltools.system("./Configure -n linux")
+
+def build():
+    autotools.make('CC="%s" DEBUG="%s" all' % (get.CC(), get.CFLAGS()))
+
+def install():
+    inarytools.dosbin("lsof")
+
+    inarytools.insinto("/usr/share/lsof/scripts", "scripts/*")
+
+    inarytools.doman("Lsof.8")
+    inarytools.dodoc("00*")
