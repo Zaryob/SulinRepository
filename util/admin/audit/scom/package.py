e13f4e8e578acf1da563b4bb846158b9de32a2dd
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-11 01:22:51 +0300

    pam ve shadow düzeltildi

--- util/admin/audit/scom/package.py
+++ util/admin/audit/scom/package.py
@@ -6,4 +6,3 @@ import os
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     os.system("ln -s /usr/sbin/audispd /sbin/audispd")
     os.system("ln -s /usr/sbin/auditd /sbin/auditd")
-    os.system("/bin/systemctl enable auditd.service")
