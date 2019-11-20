498bb6f8190fe0dbd90217e84410a29993a5366f
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-20 23:23:26 +0300

    dkms eklendi.

---
+++ kernel/tools/dkms/actions.py
@@ -0,0 +1,8 @@
+from inary.actionsapi import autotools
+from inary.actionsapi import get
+
+def build():
+    autotools.make()
+
+def install():
+	autotools.rawInstall("DESTDIR={}".format(get.installDIR()))
