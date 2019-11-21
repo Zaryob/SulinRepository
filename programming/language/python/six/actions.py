ec9f35492f966516a1e67ba2764949b2f43aab05
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-21 13:37:52 +0300

    I have worked for just johnny dep :smile:
    Python library Johnnydep compiled.

---
+++ programming/language/python/six/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from inary.actionsapi import pythonmodules
+from inary.actionsapi import inarytools
+
+def build():
+    pythonmodules.compile(pyVer="2")
+    pythonmodules.compile(pyVer="3")
+
+def install():
+    pythonmodules.install(pyVer="2")
+    pythonmodules.install(pyVer="3")
