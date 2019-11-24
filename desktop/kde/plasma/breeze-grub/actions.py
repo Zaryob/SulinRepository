f927936946ccf298fa076751289efa3fa8ba94be
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-24 20:43:04 +0000

    Plasma desktop derlendi.

---
+++ desktop/kde/plasma/breeze-grub/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from inary.actionsapi import inarytools
+
+def install():
+    inarytools.insinto("/boot/grub2/themes/breeze", "breeze/*")
+
+    inarytools.dodoc("COPYING")
