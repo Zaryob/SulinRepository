076002afedd3a9fe8607ffad60598731e9e95303
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-24 20:13:35 +0000

    Bişeyler için derlendi ama niye derlendi unuttum :smile:

---
+++ multimedia/misc/exiv2/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import cmaketools
+from inary.actionsapi import inarytools
+from inary.actionsapi import get
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
