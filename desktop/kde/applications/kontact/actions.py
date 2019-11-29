a1f177f437382474384aaedd1b2ea803321fff41
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-11-29 20:31:42 +0000

    Added most of kde  applications.

---
+++ desktop/kde/applications/kontact/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from inary.actionsapi import kde
+from inary.actionsapi import inarytools
+
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+
+    inarytools.dodoc("COPYING.DOC", "COPYING.LIB", "COPYING")
+
