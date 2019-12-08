12bb349611a35b9709e685d24801d67dc61b65e2
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-12-09 02:30:54 +0300

    cantor compiled.

---
+++ desktop/kde/applications/cantor/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+
+from inary.actionsapi import inarytools
+from inary.actionsapi import kde
+
+NoStrip=["/usr/share/icons"]
+# TODO: KAlgebra icon not shown in GUI
+# TODO: more sub packages? *-libs etc.?
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    inarytools.dodoc("COPYING*", "DESIGN*", "README*")
