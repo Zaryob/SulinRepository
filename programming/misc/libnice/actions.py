83ef783610429a0012c706fd70e4c8c87d3e9e5e
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-12-09 21:34:46 +0300

    libnice compiled.

---
+++ programming/misc/libnice/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --with-gstreamer-0.10=no")
+
+    #inarytools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    inarytools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
