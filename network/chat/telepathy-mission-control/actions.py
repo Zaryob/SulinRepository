becf65c2fb74a05429776308a22275718beb2399
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-12-11 22:10:00 +0300

    telepathy-mission-control has been compiled.
    Hudson hudson... :joy:

---
+++ network/chat/telepathy-mission-control/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import get
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --libexecdir=/usr/lib/telepathy \
+                         --enable-gtk-doc \
+                         --disable-upower \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    inarytools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "NEWS")
