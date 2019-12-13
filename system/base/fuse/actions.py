ce37e47a100d14894f7f4384d554267717132a1a
Suleyman Poyraz <zaryob.dev@gmail.com>
2019-12-13 08:12:48 +0000

    system.base packages has been autobumped.
    - Following packages have been bumped.* cpio
     * curl (revdep-rebuild)
     * e2fsprogrs (revdep-rebuild)
     * eudev
     * fuse
     * gdbm (refreshed package)
     * libpipeline
     * libseccomp
     * libsigsegv
     * man-db
     * texinfo
    
    - Refreshed index.

--- system/base/fuse/actions.py
+++ system/base/fuse/actions.py
@@ -1,41 +1,26 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from inary.actionsapi import get
-from inary.actionsapi import autotools
+from inary.actionsapi import mesontools
 from inary.actionsapi import inarytools
-from inary.actionsapi import shelltools
 
 def setup():
-    shelltools.system("sh makeconf.sh")
-    
-    # Disable device node creation during build/install
-    inarytools.dosed("util/Makefile.in", "mknod", "echo Disabled: mknod ")
-
-
-    autotools.configure("--disable-static \
-                         --disable-rpath \
-                         --enable-lib \
-                         --enable-util \
-                         --exec-prefix=/ \
-                         --bindir=/bin")
+    mesontools.meson_configure()
 
 def build():
-    autotools.make()
+    mesontools.ninja_build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.ninja_install()
 
     inarytools.removeDir("/dev")
-    inarytools.removeDir("/etc")
+ #   inarytools.removeDir("/etc")
 
     # Make compat symlinks into /usr/bin
     inarytools.dosym("/bin/fusermount", "/usr/bin/fusermount")
     inarytools.dosym("/bin/ulockmgr_server", "/usr/bin/ulockmgr_server")
 
-    # Move pkgconfig file to /usr/lib
-    inarytools.domove("/lib/pkgconfig", "/usr/lib/")
 
-    inarytools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README.NFS")
