829bf0d703e40f1ff49f099d59094de34465e0f6
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-01-09 01:41:09 +0300

    NetworkManager eksikleri ile beraber derlendi.

--- network/connection/NetworkManager/actions.py
+++ network/connection/NetworkManager/actions.py
@@ -11,33 +11,36 @@ from inary.actionsapi import shelltools
 def setup():
     shelltools.system("grep -rl '^#!.*python$' | xargs sed -i '1s/python/&3/'")
     mesontools.meson_configure("\
-          -Dlibaudit=yes             \
-          -Dlibpsl=true              \
-          -Dnmtui=true               \
-          -Dovs=true                 \
-          -Ddbus_conf_dir=/usr/share/dbus-1/system.d   \
-          -Dsession_tracking_consolekit=false    \
-          -Dmodify_system=true      \
-          -Dpolkit_agent=true       \
-          -Dselinux=false           \
-          -D config_plugins_default=keyfile \
-          -Diwd=true                \
-          -Dpppd_plugin_dir=/usr/lib/pppd/2.4.7 \
-          -Dnm_cloud_setup=true     \
-          -Dbluez5_dun=true         \
-          -Debpf=true               \
-          -Dvapi=true               \
-          -Ddocs=true               \
-          -Dmore_asserts=no         \
-          -Dmore_logging=false      \
-          -Dppp=true                 \
-          -Dselinux=false            \
-          -Dudev_dir=/lib/udev       \
-          -Dsession_tracking=elogind \
-          -Dmodem_manager=true       \
-          -Dsystemdsystemunitdir=no  \
-          -Dsystemd_journal=false    \
-          -Dqt=false                 ")
+          -Dlibaudit=yes                                \
+          -Dlibpsl=true                                 \
+          -Dnmtui=true                                  \
+          -Dovs=true                                    \
+          -Ddbus_conf_dir=/usr/share/dbus-1/system.d    \
+          -Dsession_tracking_consolekit=false           \
+          -Dmodify_system=true                          \
+          -Dpolkit_agent=true                           \
+          -Dselinux=false                               \
+          -D config_plugins_default=keyfile             \
+          -Difupdown=true                               \
+          -Difcfg-rh=true                               \
+          -Diwd=true                                    \
+          -Dpppd_plugin_dir=/usr/lib/pppd/2.4.7         \
+          -Dnm_cloud_setup=true                         \
+          -Dbluez5_dun=true                             \
+          -Debpf=true                                   \
+          -Dvapi=true                                   \
+          -Ddocs=true                                   \
+          -Dmore_asserts=no                             \
+          -Dmore_logging=false                          \
+          -Dtests=no                                 \
+          -Dppp=true                                    \
+          -Dselinux=false                               \
+          -Dudev_dir=/lib/udev                          \
+          -Dsession_tracking=elogind                    \
+          -Dmodem_manager=true                          \
+          -Dsystemdsystemunitdir=no                     \
+          -Dsystemd_journal=false                       \
+          -Dqt=false                                    ")
 
 def build():
     mesontools.ninja_build()
