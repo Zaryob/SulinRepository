16ec6526527070e61f76daa0c49a2e5cf4525161
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-01-09 15:58:44 +0300

    Issue #23 için çalışma başlatıldı.
    eksik paket derlendi sadece bağımlılık atılcak.

---
+++ multimedia/sound/pavucontrol/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Suleyman POYRAZ (Zaryob)
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl-3.0.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import get
+
+def setup():
+    autotools.autogen()
+    autotools.configure("--disable-lynx")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    inarytools.dodoc("LICENSE")
