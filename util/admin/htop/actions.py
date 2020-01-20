85781f0d141b36fc2b32af07c51aefc0ec88a231
Your Name <you@example.com>
2020-01-20 08:56:49 +0000

    htop ekledim

---
+++ util/admin/htop/actions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
+
+def setup():
+    shelltools.system("sed -i 's|ncursesw/curses.h|curses.h|' RichString.[ch] configure")
+    autotools.configure("--enable-taskstats \
+                         --enable-unicode")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    inarytools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
