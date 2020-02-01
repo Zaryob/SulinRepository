920e569dc08dfadcb6b051e708f4db83b6f0df09
Your Name <you@example.com>
2020-02-01 17:20:46 +0000

    fix glibc ldd script

--- system/base/glibc/actions.py
+++ system/base/glibc/actions.py
@@ -13,10 +13,11 @@ import os
 
 
 arch = "x86-64" if get.ARCH() == "x86_64" and not get.buildTYPE() == "emul32" else "i686"
-defaultflags = "-O3 -g -fasynchronous-unwind-tables -mtune=generic -march=%s" % arch
-if get.buildTYPE() == "emul32": defaultflags += " -m32"
+defaultflags = "-Os -g -mno-tls-direct-seg-refs -mtune=generic -march=%s" % arch
+if get.buildTYPE() == "emul32": defaultflags += " -m32 -fasynchronous-unwind-tables"
 # this is getting ridiculous, also gdb3 breaks resulting binary
 sysflags = "-mtune=generic -march=x86-64" if get.ARCH() == "x86_64" else "-mtune=generic -march=i686"
+
 ldconf32bit = """/lib32
 /usr/lib32
 """
@@ -26,25 +27,30 @@ def setup():
     shelltools.export("LANG","C")
     shelltools.export("LC_ALL","C")
 
+    shelltools.export("CC", "gcc %s " % defaultflags)
+    shelltools.export("CXX", "g++ %s " % defaultflags)
+
+    shelltools.export("CFLAGS", defaultflags)
+    shelltools.export("CXXFLAGS", defaultflags)
+
     shelltools.makedirs("build")
     shelltools.cd("build")
     options = "--prefix=/usr \
                --libdir=/usr/lib \
                --mandir=/usr/share/man \
                --infodir=/usr/share/info \
-               --libexecdir=/usr/libexec \
+               --libexecdir=/usr/lib/misc \
                --with-bugurl=http://gitlab.com/sulinos/main/issues \
                --enable-add-ons \
                --enable-bind-now \
-               --enable-cet \
                --enable-lock-elision \
                --enable-multi-arch \
                --enable-static-pie\
-               --enable-stack-protector=strong \
+               --enable-stack-protector=all \
+               --enable-kernel=5.4.0 \
+               --with-headers=/usr/include \
                --enable-stackguard-randomization \
-               --disable-profile \
                --disable-werror"
-
     if get.buildTYPE() == "emul32":
         options += "\
                     --libdir=/usr/lib32 \
@@ -59,10 +65,10 @@ def build():
         shelltools.echo("configparms","build-programs=no")
         shelltools.echo("configparms", "slibdir=/lib32")
         shelltools.echo("configparms", "rtlddir=/lib32")
-        shelltools.echo("configparms", "bindir=/bin")
-        shelltools.echo("configparms", "sbindir=/sbin")
-        shelltools.echo("configparms", "rootsbindir=/sbin")
-        shelltools.echo("configparms", "datarootdir=/usr/share")
+        shelltools.echo("configparms", "bindir=/tmp32")
+        shelltools.echo("configparms", "sbindir=/tmp32")
+        shelltools.echo("configparms", "rootsbindir=/tmp32")
+        shelltools.echo("configparms", "datarootdir=/tmp32/share")
 
         autotools.make()
 
@@ -74,13 +80,6 @@ def build():
 
     autotools.make()
 
-def check():
-     shelltools.cd("build")
-
-     if get.buildTYPE() != "emul32":
-        autotools.make("check || true")
-     else:
-        pass
 
 def install():
     shelltools.cd("build")
@@ -93,8 +92,6 @@ def install():
         #Install locales once.
         autotools.rawInstall("install_root=%s localedata/install-locales" % get.installDIR())
 
-        # Remove our options section from crt stuff
-        #removeSulinLinuxSection("%s/usr/lib/" % get.installDIR())
 
 
     if get.buildTYPE() == "emul32":
@@ -102,6 +99,7 @@ def install():
 
         shelltools.echo("%s/etc/ld.so.conf.d/60-glibc-32bit.conf" % get.installDIR(), ldconf32bit)
 
+        inarytools.removeDir("/tmp32")
 
 
     # We'll take care of the cache ourselves
@@ -128,3 +126,4 @@ def install():
 
     shelltools.cd("..")
     inarytools.dodoc("ChangeLog", "NEWS", "README*", "LICENSES")
+
