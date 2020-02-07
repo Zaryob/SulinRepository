73c6d732a96776e79360be1044d1f0ae50edd25f
Your Name <you@example.com>
2020-02-07 23:14:23 +0000

    Åxorg-font düzenlemesi

--- x11/misc/xorg-font/scom/package.py
+++ x11/misc/xorg-font/scom/package.py
@@ -5,7 +5,6 @@ import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     try:
-
         # Trailing slash is important since it is used with dosed.
         encodingsDir = "/usr/share/fonts/encodings/"
         # Font scaling
@@ -16,6 +15,8 @@ def postInstall(fromVersion, fromRelease, toVersion, toRelease):
         os.system("/usr/bin/mkfontscale -b -s -l -n -r -p {} -e . -e large .".format(encodingsDir))
         os.system("/usr/bin/mkfontdir -n -e {0} -e {0}large .".format(encodingsDir))
         os.system("sed -i 's|{}||' encodings.dir ".format(encodingsDir))
+        #fontdir generate
+        os.system("/usr/bin/mkfontdir /usr/share/fonts/*")
 
     except:
         pass
