1d5a5ab3b37382a8e7e1b6e2e61b445a0a075f52
Your Name <you@example.com>
2020-02-21 08:34:32 +0000

    cairomm updated

--- desktop/toolkit/gnome/gnome-themes-yaru/actions.py
+++ desktop/toolkit/gnome/gnome-themes-yaru/actions.py
@@ -4,15 +4,21 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from inary.actionsapi import mesontools
 from inary.actionsapi import shelltools
 from inary.actionsapi import inarytools
+from inary.actionsapi import get
+import os
+
+WORKDIR="yaru-20.04.1"
 
 def setup():
-    shelltools.system("meson build")
+    shelltools.system("mkdir build")
 
 def build():
-    mesontools.ninja_build()
+    _env=os.environ.copy()
+    os.environ.clear()
+    shelltools.system("meson build")
+    os.environ.update(_env)
 
 def install():
-    mesontools.ninja_install()
+    shelltools.system("cd build ; DESTDIR='{}' ninja install".format(get.installDIR()))
--- system/base/baselayout/postoperations.py
+++ system/base/baselayout/postoperations.py
@@ -6,12 +6,12 @@ def create_dir(name):
     if os.path.exists(name)==False:
         os.system("mkdir -p "+name)
 def copy_file(source,target,perm):
-	os.system("cp -prf '"+source"' '"+target"'")
+	os.system("cp -prf '"+source+"' '"+target+"'")
 	os.chmod(target,perm)
 
 def postInstall():
-	"""base config"""
-	create_dir("/etc/bash/")
+    """base config"""
+    create_dir("/etc/bash/")
     create_dir("/etc/bash/bashrc.d")
     create_dir("/usr/share/icons/hicolor/")
     specialFiles = ["passwd", "locale.conf", "group", "fstab", "ld.so.conf", "resolv.conf"]
@@ -24,7 +24,7 @@ def postInstall():
     copy_file("/usr/share/baselayout/inittab.openrc", "/etc/inittab",0o700)
     copy_file("/usr/share/baselayout/bash/bashrc","/etc/bash/bashrc",0o755)
     for i in os.listdir("/usr/share/baselayout/bash/bashrc.d"):
-	    copy_file("/usr/share/baselayout/bash/bashrc.d/"+i, "/etc/bash/bashrc.d"+i,0o755)
+	    copy_file("/usr/share/baselayout/bash/bashrc.d/"+i, "/etc/bash/bashrc.d/"+i,0o755)
     copy_file("/usr/share/baselayout/shadow", "/etc/shadow",0o700)
     copy_file("/usr/share/baselayout/index.theme", "/usr/share/icons/hicolor/index.theme",0o755)
     os.chmod("/usr/share/icons/hicolor/index.theme",0o755)
