006aae651b88c6d9e1aec3ca5da3cb1904fc11eb
Your Name <you@example.com>
2020-03-03 19:11:56 +0000

    mate derlemesi basladi

---
+++ desktop/mate/base/atril/actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh")
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --localstatedir=/var \
+                         --disable-static \
+                         --disable-rpath \
+                         --enable-pdf \
+                         --enable-tiff \
+                         --enable-djvu \
+                         --enable-dvi \
+                         --enable-xps \
+                         --enable-t1lib \
+                         --enable-comics \
+                         --enable-pixbuf \
+                         --enable-impress \
+                         --with-gtk=3.0 ")
+    
+    # for fix unused dependency
+    inarytools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    inarytools.dodoc("README", "NEWS", "AUTHORS", "COPYING")
