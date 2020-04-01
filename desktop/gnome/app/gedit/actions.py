c83b461b95167fe0e1b77a9faaa766ecf0254a69
sulincix <parduscix@yandex.ru>
2020-04-01 16:07:44 +0000

    fileroller tamam

--- desktop/gnome/app/gedit/actions.py
+++ desktop/gnome/app/gedit/actions.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
+# Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from inary.actionsapi import autotools
 from inary.actionsapi import mesontools
+from inary.actionsapi import cmaketools
 from inary.actionsapi import inarytools
 from inary.actionsapi import get
 
-
 def setup():
-    mesontools.meson_configure("-D_systemd=false")
+    mesontools.meson_configure()
 
 def build():
     mesontools.ninja_build()
 
 def install():
     mesontools.ninja_install()
-
-    inarytools.dodoc("AUTHORS", "ChangeLog.*","COPYING.*", "NEWS", "README.md")
