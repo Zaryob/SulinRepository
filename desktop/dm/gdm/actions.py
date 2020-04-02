596879c18924394962a5ee442197ceec3aadcc63
sulincix <parduscix@yandex.ru>
2020-04-02 11:52:30 +0000

    file-roller ve engrampa sorunu çözüldü

--- desktop/dm/gdm/actions.py
+++ desktop/dm/gdm/actions.py
@@ -13,19 +13,23 @@ def setup():
     shelltools.export("SYSTEMD_LIBS","-lelogind")
     shelltools.export("SYSTEMD_CFLAGS","-I/usr/include/elogind")
     shelltools.system("NOCONFIGURE=1 ./autogen.sh")
-    autotools.configure("--without-plymouth    \
-            --disable-static      \
-            --enable-gdm-xsession \
-            --with-pam-mod-dir=/lib/security\
-            --without-xevie\
-            --without-plymouth \
-            --without-tcp-wrappers\
-            --enable-authentication-scheme=pam\
-            --with-systemdsystemunitdir=no")
+    autotools.configure("--without-plymouth \
+		--with-initial-vt=1 \
+		--enable-wayland-support \
+		--without-tcp-wrappers \
+		--with-pid-file=/var/run/gdm.pid \
+		--enable-authentication-scheme=pam \
+		--with-default-pam-config=arch \
+		--with-log-dir=/var/log/gdm \
+--with-pam-mod-dir=/lib/security")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.system('sed -i "s/pam_systemd\.so/pam_elogind.so/" {}/etc/pam.d/*'.format(get.installDIR()))
+    f=open('{}/etc/gdm/Xsession'.format(get.installDIR()),"w")
+    f.write('#!/bin/sh\n')
+    f.write('exec "$@"')
 
