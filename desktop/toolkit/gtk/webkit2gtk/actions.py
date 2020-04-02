d6878b36244d5bff580bf91fae485677340e9a9a
sulincix <parduscix@yandex.ru>
2020-04-02 15:42:14 +0000

    webkit2gtk düzenlemesi

--- desktop/toolkit/gtk/webkit2gtk/actions.py
+++ desktop/toolkit/gtk/webkit2gtk/actions.py
@@ -17,17 +17,16 @@ docs = ["AUTHORS", "COPYING.LIB", "THANKS", \
 def setup():
     shelltools.system("mkdir build")
     shelltools.cd("build")
-    shelltools.system(" cmake -DCMAKE_BUILD_TYPE=Release  \
-      -DCMAKE_INSTALL_PREFIX=/usr \
-      -DCMAKE_SKIP_RPATH=ON       \
-      -DPORT=GTK                  \
-      -DLIB_INSTALL_DIR=/usr/lib  \
-      -DUSE_LIBHYPHEN=OFF         \
-      -DENABLE_MINIBROWSER=ON     \
-      -DUSE_WOFF2=OFF             \
-      -DUSE_WPE_RENDERER=OFF      \
-      -DENABLE_BUBBLEWRAP_SANDBOX=OFF \
-      -Wno-dev -G Ninja ..")
+    shelltools.system(" cmake \
+    -DPORT=GTK \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_SKIP_RPATH=ON \
+    -DENABLE_GTKDOC=ON \
+    -DENABLE_MINIBROWSER=ON \
+    -DLIBEXEC_INSTALL_DIR=/usr/lib \
+    -DLIB_INSTALL_DIR=/usr/lib\
+    -G Ninja ..")
 
 def build():
     shelltools.cd("build")
