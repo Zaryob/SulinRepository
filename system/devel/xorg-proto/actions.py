00fcccdb86674abb9e5304653766a422d1c4ee1f
sulincix <parduscix@yandex.ru>
2020-04-02 14:44:37 +0000

    xorg-proto

--- system/devel/xorg-proto/actions.py
+++ system/devel/xorg-proto/actions.py
@@ -1,21 +1,20 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from inary.actionsapi import autotools
+from inary.actionsapi import mesontools
+from inary.actionsapi import cmaketools
 from inary.actionsapi import inarytools
-from inary.actionsapi import shelltools
 from inary.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vif")
-    autotools.configure("--prefix=/usr \
-                         --docdir=/usr/share/doc/xorg-proto")
-    
-def build():
-    autotools.make()
-    
-def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    mesontools.meson_configure()
 
+def build():
+    mesontools.ninja_build()
+
+def install():
+    mesontools.ninja_install()
