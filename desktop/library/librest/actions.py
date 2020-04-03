3c8ca118769ba686fc16aca28b2417aa1eaa6264
sulincix <parduscix@yandex.ru>
2020-04-03 12:28:23 +0000

    gnome-ayarları tamam

---
+++ desktop/library/librest/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
+
+def setup():
+    options = "--prefix=/usr \
+               --sysconfdir=/etc"
+               
+    if get.buildTYPE() == "_emul32":
+        options += " --libdir=/usr/lib32 \
+                     --bindir=/_emul32/bin \
+                     --sbindir=/_emul32/sbin \
+                     --disable-dtrace"
+                     
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+        shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+        
+    autotools.configure(options)
+
+    inarytools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    inarytools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
