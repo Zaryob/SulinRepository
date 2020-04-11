9622ee1da89179eef21fe4e5dfaeac6d3aad1418
Ali Rıza KESKİN <parduscix@yandex.ru>
2020-04-11 23:16:28 +0000

    gimp derlendi

---
+++ multimedia/graphics/mypaint-brushes/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import shelltools
+from inary.actionsapi import inarytools
+from inary.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
