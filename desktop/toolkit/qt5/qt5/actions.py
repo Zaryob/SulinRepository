b35584d083d334cf63a3995a363657ffcc182918
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-05-18 22:13:35 +0300

    qt5 deneme

--- desktop/toolkit/qt5/qt5/actions.py
+++ desktop/toolkit/qt5/qt5/actions.py
@@ -4,8 +4,8 @@ from inary.actionsapi import inarytools
 from inary.actionsapi import qt
 from inary.actionsapi import get
 
-WorkDir = "qt-everywhere-src-%s" % get.srcVERSION().replace('_','-').replace('pre1', 'tp')
-absoluteWorkDir = "%s/%s" % (get.workDIR(), WorkDir)
+WorkDir = "qt-everywhere-src-{}".format(get.srcVERSION().replace('_','-').replace('pre1', 'tp'))
+absoluteWorkDir = "{}/{}".format(get.workDIR(), WorkDir)
 
 def setup():
     filteredCFLAGS = get.CFLAGS().replace("-g3", "-g")
@@ -17,6 +17,19 @@ def setup():
     shelltools.export("CXXFLAGS", filteredCXXFLAGS)
     shelltools.system('parameters=\"QMAKE_CFLAGS_ISYSTEM= \"')
     shelltools.system("OPENSSL_LIBS=\'-L/usr/lib -lssl -lcrypto\'  ../configure -confirm-license -opensource \
+                            -prefix {} \
+                            -libdir {} \
+                            -docdir {} \
+                            -examplesdir {} \
+                            -demosdir {}\
+                            -plugindir {} \
+                            -translationdir {} \
+                            -sysconfdir {} \
+                            -datadir {} \
+                            -importdir {} \
+                            -headerdir {} \
+                            -moduledir {} \
+                            -confirm-license \
                             -prefix /usr \
                             -docdir /usr/share/doc/qt5 \
                             -plugindir /usr/lib/qt5/plugins \
@@ -28,6 +41,7 @@ def setup():
                             -examplesdir /usr/lib/qt5/examples \
                             -system-sqlite \
                             -system-zlib \
+                            -plugin-sql-{psql,mysql,sqlite,odbc,ibase} \
                             -icu \
                             -ccache \
                             -webengine-icu \
@@ -44,11 +58,22 @@ def setup():
                             -reduce-relocations \
                             -no-separate-debug-info \
                             -opengl \
-                            -glib ")
+                            -glib ".format(qt.prefix,
+                                           qt.libdir,
+                                           qt.docdir,
+                                           qt.examplesdir,
+                                           qt.demosdir,
+                                           qt.plugindir,
+                                           qt.sysconfdir,
+                                           qt.translationdir,
+                                           qt.datadir,
+                                           qt.importdir,
+                                           qt.includedir,
+                                           qt.moduledir))
 def build():
     shelltools.cd("build")
     autotools.make()
 
 def install():
     shelltools.cd("build")
-    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+    autotools.rawInstall("INSTALL_ROOT={}".format(get.installDIR()))
