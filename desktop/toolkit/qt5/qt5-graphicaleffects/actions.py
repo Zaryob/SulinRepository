ba79f20aad29de28fb424c603a6a17746b5be918
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-06-08 18:06:01 +0000

    qt5 yeniden dagitildi
    birlesik derlenmio haci bu

---
+++ desktop/toolkit/qt5/qt5-graphicaleffects/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import shelltools
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import qt
+from inary.actionsapi import get
+
+def setup():
+    qt.configure()
+
+def build():
+    qt.make()
+    qt.make("docs")
+
+def install():
+    qt.install("INSTALL_ROOT=%s install_docs" % get.installDIR())
+
+    #I hope qtchooser will manage this issue
+    for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
+        inarytools.dosym("/usr/lib/qt5/bin/%s" % bin, "/usr/bin/%s-qt5" % bin)
+
+    inarytools.insinto("/usr/share/licenses/qt5-graphicaleffects/", "LICENSE*")
