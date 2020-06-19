83ff44be168db3922018f6975baa227c9df43e54
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-06-19 16:32:21 +0000

    Fixed with new kerneltools module.

--- kernel/linux-libre/actions.py
+++ kernel/linux-libre/actions.py
@@ -26,24 +26,10 @@ def build():
 
 
 def install():
-    #empity dir for buggy kerneltools
-    inarytools.dodir("/lib/modules/5.7.2-sulinos/source")
-    inarytools.dodir("/lib/modules/5.7.2-sulinos/build")
-    kerneltools.install()
-    inarytools.removeDir("/lib/modules/5.7.2-sulinos/source")
-    inarytools.removeDir("/lib/modules/5.7.2-sulinos/build")
-    inarytools.removeDir("/usr/bin")
-    kerneltools.installHeaders()
-    kerneltools.installLibcHeaders()
-    #fix libre kernel suffix bug
-    shelltools.system("mv {0}/boot/linux-5.7.2 {0}/boot/linux-5.7.2-libre-sulinos".format(get.installDIR()))
-    shelltools.system("mv {0}/usr/src/libre-headers-5.7.2-sulinos {0}/usr/src/linux-headers-5.7.2-libre-sulinos".format(get.installDIR()))
-    shelltools.system("mv {0}/lib/modules/5.7.2-sulinos {0}/lib/modules/5.7.2-libre-sulinos".format(get.installDIR()))
+    kerneltools.install(distro="libre-sulinos")
+    kerneltools.installModuleHeaders(distro="libre-sulinos")
     # add objtool for external module building and enabled VALIDATION_STACK option
     inarytools.insinto("/usr/src/linux-headers-%s-libre-sulinos/tools/objtool" % get.srcVERSION(), "%s/tools/objtool/objtool" % get.curDIR())
 
     # Generate some module lists to use within mkinitramfs
     shelltools.system("./generate-module-list %s/lib/modules/%s-libre-sulinos" % (get.installDIR(), kerneltools.__getSuffix()))
-    #kernel include dirs from linux
-    shelltools.system("rm -rf  {0}/usr/include".format(get.installDIR()))
-
