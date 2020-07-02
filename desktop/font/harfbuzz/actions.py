dbca3b93ee5e42d9fbfdcc8a08537500664e3bfb
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-07-02 22:53:09 +0000

    harfbuzz yeniden derlendi. 2.68.2 sürümüne yükseltildi.

--- desktop/font/harfbuzz/actions.py
+++ desktop/font/harfbuzz/actions.py
@@ -3,40 +3,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
-from inary.actionsapi import get
-from inary.actionsapi import autotools
+from inary.actionsapi import mesontools
 from inary.actionsapi import inarytools
-from inary.actionsapi import shelltools
+from inary.actionsapi import get
 
-if get.buildTYPE() == "emul32":
-   shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+
 def setup():
-    autotools.autoreconf("-ivf")
-
-    if get.buildTYPE() == "emul32":
-        options = "--with-glib=yes \
-                    --with-graphite2=no \
-                    --with-cairo=yes \
-                    --with-icu=yes"
-
-    else:
-        options = "--with-glib=yes \
-                   --with-freetype=yes \
-                   --with-cairo=yes \
-                   --with-icu=yes \
-                   --with-gobject=yes \
-                   --with-graphite2=yes"
-
-
-    autotools.configure(options)
-
-    inarytools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.meson_configure("-D graphite=disabled" if get.buildTYPE() == "emul32" else "-D graphite=enabled" )
 
 def build():
-    autotools.make()
-
+    mesontools.ninja_build()
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
+    mesontools.ninja_install()
+    if get.buildTYPE()=="emul32":
+        inarytools.domove("/usr/*.so*", "/usr/lib32")
+        inarytools.domove("/usr/cmake", "/usr/lib32")
+        inarytools.domove("/usr/girepository-1.0", "/usr/lib32")
+        inarytools.domove("/usr/pkgconfig", "/usr/lib32")
+        return
     inarytools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
-
