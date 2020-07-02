2b8e50719e3b5c5cf1321d84857d60f0c66b1c34
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-07-02 21:20:10 +0000

    orc yeniden derlendi. Sürümü 0.30.1 oldu.

--- multimedia/misc/orc/actions.py
+++ multimedia/misc/orc/actions.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from inary.actionsapi import get
 from inary.actionsapi import mesontools
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+
 
 def setup():
     mesontools.meson_configure()
@@ -14,3 +17,7 @@ def build():
 
 def install():
     mesontools.ninja_install()
+    if get.buildTYPE()=="emul32":
+        inarytools.domove("/usr/*.so*", "/usr/lib32/")
+        inarytools.domove("/usr/liborc*.a", "/usr/lib32/")
+        inarytools.domove("/usr/pkgconfig", "/usr/lib32/")
