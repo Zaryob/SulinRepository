525ba22ad07351d12e39a269f184125dacc41697
sulincix <parduscix@yandex.ru>
2020-07-17 09:36:30 +0000

    update

---
+++ desktop/xfce/plugin/xfce4-whisker-plugin/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from inary.actionsapi import cmaketools
+from inary.actionsapi import get
+from inary.actionsapi import inarytools
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
