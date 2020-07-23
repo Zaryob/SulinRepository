23b9e6a583f3595ae4394c33081af7924321287f
sulincix <parduscix@yandex.ru>
2020-07-23 13:42:35 +0000

    gnome-builder done

--- desktop/toolkit/gtk/glade/actions.py
+++ desktop/toolkit/gtk/glade/actions.py
@@ -4,23 +4,15 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from inary.actionsapi import autotools
+from inary.actionsapi import mesontools
 from inary.actionsapi import inarytools
 from inary.actionsapi import get
 
-WorkDir = "glade3-%s" % get.srcVERSION()
-
 def setup():
-    autotools.configure("PYTHON=/usr/bin/python3 \
-                         --disable-static \
-                         --enable-gtk-doc")
+    mesontools.meson_configure()
 
 def build():
-    autotools.make()
+    mesontools.ninja_build()
 
 def install():
-    autotools.install()
-
-    inarytools.remove("/usr/share/icons/hicolor/icon-theme.cache")
-
-    inarytools.dodoc("AUTHORS", "COPYING*", "ChangeLog", "NEWS", "README", "TODO")
+    mesontools.ninja_install()
