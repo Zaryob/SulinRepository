1c9c2298154f31a3cdd56c95ec955a3a0a0c32c1
Your Name <you@example.com>
2020-09-20 13:13:15 +0000

    vulkan 32bit

--- x11/library/vulkan-icd-loader/actions.py
+++ x11/library/vulkan-icd-loader/actions.py
@@ -6,14 +6,21 @@
 
 from inary.actionsapi import cmaketools
 from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
 from inary.actionsapi import get
 
 WorkDir="Vulkan-Loader-1.2.153"
 
 def setup():
+    if get.buildTYPE() == "emul32":
+        shelltools.export("CC","gcc -m32")
+        shelltools.export("CXX","g++ -m32")
+        shelltools.export("LDFLAGS","-m32")
+        cfg="-DCMAKE_INSTALL_LIBDIR=lib32"
+    else:
+        cfg="-DCMAKE_INSTALL_LIBDIR=lib"
     cmaketools.configure("--DCMAKE_INSTALL_PREFIX=/usr \
     -DVULKAN_HEADERS_INSTALL_DIR=/usr \
-    -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_SYSCONFDIR=/etc \
     -DCMAKE_INSTALL_DATADIR=/share \
     -DCMAKE_SKIP_RPATH=True \
@@ -21,7 +28,7 @@ def setup():
     -DBUILD_WSI_XCB_SUPPORT=On \
     -DBUILD_WSI_XLIB_SUPPORT=On \
     -DBUILD_WSI_WAYLAND_SUPPORT=On \
-    -DCMAKE_BUILD_TYPE=Release ")
+    -DCMAKE_BUILD_TYPE=Release "+cfg)
 
 def build():
     cmaketools.make()
