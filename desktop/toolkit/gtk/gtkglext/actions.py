97723bb83ad93741ee679dc446089a37e5c50634
Your Name <you@example.com>
2020-11-12 05:58:31 +0000

    gtkglext package created

---
+++ desktop/toolkit/gtk/gtkglext/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from inary.actionsapi import autotools
+from inary.actionsapi import inarytools
+from inary.actionsapi import shelltools
+from inary.actionsapi import get
+def setup():
+
+    shelltools.system(" autoreconf -fvi")
+    autotools.configure("--prefix=/usr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR={}".format(get.installDIR()))
+
+    inarytools.dodoc("ChangeLog","COPYING", "README")
