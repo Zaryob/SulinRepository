ee8ed57f412d89701664429a1929ee5376ccd532
Adınız <siz@e-posta.com>
2021-02-23 18:57:56 +0000

    pam downgrade

--- system/base/pam/actions.py
+++ system/base/pam/actions.py
@@ -8,46 +8,38 @@ from inary.actionsapi import autotools
 from inary.actionsapi import inarytools
 from inary.actionsapi import shelltools
 from inary.actionsapi import get
-import os
 
-def resetenv():
-    os.environ.clear()
-    os.environ["PATH"]="/bin:/usr/bin:/usr/sbin:/sbin"
-    
-def run(cmd):
-    print(cmd)
-    i=os.system(cmd)
-    if i!=0:
-        exit(i)
 def setup():
-    lib=("32" if get.buildTYPE()=="emul32" else "")
-    resetenv()
-    if lib =="32":
-        os.environ["CFLAGS"]="-m32"
-        os.environ["CXXFLAGS"]="-m32"
-        os.environ["LDFLAGS"]="-m32"
-    run("sed -e 's/pam_rhosts//g' -i modules/Makefile.am")
-    run(" autoreconf -fvi")
-    run("./configure --libdir=/usr/lib{0} \
-                         --disable-nls \
-                         --disable-db \
-                         --disable-selinux \
+    inarytools.flags.add("-fPIC -D_GNU_SOURCE")
+
+    autotools.autoreconf("-fi")
+    autotools.configure("--libdir=/usr/lib{0} \
+                         --enable-nls \
+                         --enable-audit \
                          --enable-securedir=/lib{0}/security \
-                         --enable-isadir='.'".format(lib))
+                         --enable-isadir='.'".format("32" if get.buildTYPE()=="emul32" else ""))
+
+    inarytools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
-    resetenv()
+    # Update .po files
+    autotools.make("-C po update-gmo")
 
-    os.system("make")
+    autotools.make()
 
 def check():
-    resetenv()
-    run("make check")
+    autotools.make("check")
 
     # dlopen check
-    run("./dlopen-test.sh")
+    shelltools.system("./dlopen-test.sh")
+    pass
 
 def install():
-    pkgdir=get.installDIR()
-    resetenv()
-    run("make install DESTDIR=%s" % pkgdir)
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    if get.buildTYPE()=="emul32":
+        return
+
+    inarytools.removeDir("/usr/share/doc/Linux-PAM/")
+
+    inarytools.doman("doc/man/*.[0-9]")
+    inarytools.dodoc("CHANGELOG", "Copyright", "README")
