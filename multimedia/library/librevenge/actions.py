99c0926b04ba844df3177930d2919b1624af8297
Ali Rıza KESKİN <parduscix@yandex.ru>
2021-05-05 23:36:56 +0000

    inkscape update

---
+++ multimedia/library/librevenge/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+from inary.actionsapi import inarytools
+from inary.actionsapi import autotools as tools
+from inary.actionsapi import get
+#more information : https://gitlab.com/sulinos/inary/tree/master/inary/actionsapi
+def setup():
+    tools.configure("--disable-werror")
+def build():
+    tools.make()
+
+def install():
+    tools.rawInstall("DESTDIR=%s" % get.installDIR())
+
